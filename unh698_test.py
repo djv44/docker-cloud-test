f11f503354d182096151f8c5ff57ffc7cb6c4b23
daniel beitel <daniel beitel>
2017-04-17 19:59:25 -0400

    new page test 1

--- unh698_test.py
+++ unh698_test.py
@@ -17,6 +17,18 @@ class FlaskrTestCase(unittest.TestCase):
         # Chech that the page contians the desired phrase
         assert b'UNH698 Website' in rv.data
 
+    def test_link_to_my_page(self):
+        rv = self.app.get('/')
+    #search the page contents for the link to your topic
+    #replace xxxxxxxxxx with text you epect to see on main page that links to subpage
+        assert b'xxxxxxxxxxxx' in rv.data
+
+    def test_my_topic(self):
+    #replace '/' with the page path you want to make
+        rv = self.app.get('/')
+    #replace UNH698 website with the text you expect to see on topic page
+        assert b'UNH698 Website' in rv.data          
+
 
 if __name__ == '__main__':
     unittest.main()
